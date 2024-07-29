18a8ab2bf54f22396aaee834ba9944df3f7e955e
chowzhenyu <chowzheny@gmail.com>
2024-07-29 22:06:23 +0800

    SingleFileCmakeTest

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# CmakeTutoria
+# CmakeTutorial
 
 ## Cmake概述
---
+++ SingleFile/CMakeFiles/SingleFile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleFile.dir/main.cc.o"
+  "CMakeFiles/SingleFile.dir/main.cc.o.d"
+  "SingleFile"
+  "SingleFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SingleFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
